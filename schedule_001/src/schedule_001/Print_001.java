159127671d19e8229d7c655c8921ea8dc9f792c5
h473r560nn4h473 <hatersgonnahate616@gmail.com>
2024-09-15 19:48:08 +0900

    fix: java.util.InputMismatchException

--- schedule_001/src/schedule_001/Print_001.java
+++ schedule_001/src/schedule_001/Print_001.java
@@ -5,6 +5,7 @@ import java.nio.file.Files;     // 파일 존재유무, 생성
 import java.nio.file.Paths;     // Files 라이브러리 보조, 파일 경로 확인
 import java.util.List;          // 할 일 목록, 상태
 import java.util.ArrayList;     // List 라이브러리 보조, 동적 배열
+import java.util.InputMismatchException;    // 잘못된 입력값으로 발생할 수 있는 InputMismatchException 예외 처리
 import java.io.FileReader;      // 파일 내용 읽어오기, 파일을 읽기 위해 바이트 스트림을 문자 스트림으로 변환, 한번에 한 문자만 읽어옴
 import java.io.BufferedReader;  // FileReader 보조, 버퍼를 사용하여 많은 데이터를 한 번에 읽어옴
 import java.io.FileWriter;      // 파일 내용 쓰기, 파일에 데이터를 쓰기 위해 문자 스트림을 바이트 스트림으로 변환, 한번에 한 문자만 씀
@@ -14,7 +15,7 @@ import java.util.Scanner;       // 사용자 입력 처리
 
 public class Print_001 {
   private static final String FILE_NAME = "schedule_001.txt";   // 파일 이름
-  private static List<String> stuffs = new ArrayList<>();        // 할일 목록
+  private static List<String> stuffs = new ArrayList<>();       // 할일 목록
   private static List<Boolean> stuffStatus = new ArrayList<>(); // 할일 상태
 
   public static void main(String[] args) {
@@ -43,7 +44,7 @@ public class Print_001 {
     try (BufferedReader reader = new BufferedReader(new FileReader(FILE_NAME))) {   // new FileReader(FILE_NAME)는 파일에서 문자를 읽어오는 클래스인 FileReader 객체를 생성하여 지정된 파일(FILE_NAME)을 읽고, new BufferedReader(new FileReader(FILE_NAME))는 BufferedReader 객체를 생성하여 FileReader를 감싼다(FileReader를 포함하여 그 기능을 확장한다). FileReader를 사용하여 파일에서 데이터를 읽어오지만, 내부적으로 버퍼링을 통해 더 효율적으로 데이터를 처리할 수 있게 된다. 
       String line;                                      // 할일을 한줄마다 저장하기 위한 변수를 선언
       while ((line = reader.readLine()) != null) {      // BufferedReader 객체의 메소드인 reader.readLine()으로 파일을 한 줄씩 읽고 line 변수에 저장한다. reader.readLine()은 일의 끝에 도달할 경우 null을 반환하는데, 반복문이 참일 조건이 null이 아닐 때이므로 반복문은 파일의 끝에 도달할 때까지 반복된다.  
-        stuffs.add(line);                                // 할일 목록을 저장하는 동적 배열인 stuffs에 파일에서 읽어온 할일을 한 줄씩 저장
+        stuffs.add(line);                               // 할일 목록을 저장하는 동적 배열인 stuffs에 파일에서 읽어온 할일을 한 줄씩 저장
         stuffStatus.add(false);                         // 기본 할일 상태를 체크되지 않음으로 설정
       }
     } catch (IOException e) {
@@ -52,11 +53,12 @@ public class Print_001 {
   }
 
   private static void listStuff() {
-    for (int i = 0; i < stuffs.size(); i++) {            // 할일 목록에 줄 번호와 상태를 추가하는 반복문
+    for (int i = 0; i < stuffs.size(); i++) {           // 할일 목록에 줄 번호와 상태를 추가하는 반복문
       String status = stuffStatus.get(i) ? "■" : "□";   // 할일 상태 표시
       System.out.println((i + 1) + ". " + status + " " + stuffs.get(i)); // 할일 줄 번호(컴퓨터는 숫자를 0부터 세기 시작하므로 1을 더해줘야 한다), 상태, 할일 내용 출력
     }
     System.out.println("\n---\n");
+    System.out.println("할일을 저장하려면 메인메뉴에서 프로그램 종료를 선택해서 종료해야 합니다.");
     System.out.println("숫자를 입력하고 Enter키로 커맨드 실행:");
     System.out.println("1. 체크 / 체크해제");
     System.out.println("2. 할일 내용 변경");
@@ -85,8 +87,23 @@ public class Print_001 {
   }
 
   private static void toggleStuff(Scanner scanner) {    // Scanner 객체를 매개변수로 받아 할일의 체크 상태를 변경한다
-    System.out.println("체크/체크해제할 할일 번호를 입력하세요:");
-    int stuffNumber = scanner.nextInt() - 1;    // 사용자가 입력한 번호를 읽어와 stuffNumber 변수에 저장한다. 인덱스는 0부터 시작하므로 1을 빼줘야 한다.
+    int stuffNumber = -1; // 사용자가 입력한 번호를 읽어와 stuffNumber 변수에 저장한다. -1로 초기화하는 이유는 유효한 입력을 받기 전까지 taskNumber가 유효하지 않은 상태임을 나타내기 위해서이다.
+    while(true) {
+      try {
+        System.out.println("체크/체크해제할 할일 번호를 입력하세요:");
+        stuffNumber = scanner.nextInt();
+        if (stuffNumber < 1 || stuffNumber > 10) {
+            System.out.println("할일은 최대 10개까지만 저장할 수 있습니다.");
+            continue;
+        }
+        stuffNumber--; // 리스트 인덱스는 0부터 시작하므로 1을 빼준다.
+        break;
+      } catch(InputMismatchException e) {
+        System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+        scanner.next(); // 직전에 입력한 값이 버퍼에 남아있으므로 한번 소비해줘야 한다.
+      }
+    }
+    
     if (stuffNumber >= 0 && stuffNumber < stuffs.size()) {   // 유효한 줄 번호인지 확인
       stuffStatus.set(stuffNumber, !stuffStatus.get(stuffNumber));  // 체크 상태 변경
       listStuff();  // 변경된 할일 목록 출력
@@ -116,9 +133,23 @@ public class Print_001 {
   }
 
   private static void changeStuff(Scanner scanner) {
-    System.out.println("변경할 할일 번호를 입력하세요:");
-    int stuffNumber = scanner.nextInt() - 1;    // 사용자가 입력한 번호를 읽어와 stuffNumber 변수에 저장한다. 인덱스는 0부터 시작하므로 1을 빼줘야 한다.
-    scanner.nextLine(); // 바로 전 과정에서 Enter를 입력하여 버퍼에 개행 문자가 남아있으므로 개행 문자를 한번 소비해줘야 한다.
+    int stuffNumber = -1; // 사용자가 입력한 번호를 읽어와 stuffNumber 변수에 저장한다. -1로 초기화하는 이유는 유효한 입력을 받기 전까지 taskNumber가 유효하지 않은 상태임을 나타내기 위해서이다.
+    while(true) {
+      try {
+        System.out.println("변경할 할일 번호를 입력하세요:");
+        stuffNumber = scanner.nextInt();
+        if (stuffNumber < 1 || stuffNumber > 10) {
+            System.out.println("할일은 최대 10개까지만 저장할 수 있습니다.");
+            continue;
+        }
+        stuffNumber--; // 리스트 인덱스는 0부터 시작하므로 1을 빼준다.
+        break;
+      } catch(InputMismatchException e) {
+        System.out.println("잘못된 입력입니다. 숫자를 입력하세요.");
+        scanner.next(); // 직전에 입력한 값이 버퍼에 남아있으므로 한번 소비해줘야 한다. 
+      }
+    }
+
     if (stuffNumber >= 0 && stuffNumber < stuffs.size()) {   // 입력한 번호가 유효한 줄 번호인지 확인
       System.out.println("기존 할일 내용: " + stuffs.get(stuffNumber));    // 입력한 번호가 유효한 줄 번호일 경우 기존 할일 내용을 출력
       System.out.println("새 할일 내용을 입력하세요:");
@@ -128,6 +159,7 @@ public class Print_001 {
       listStuff();  // 변경된 할일 목록 출력
     } else {    // 빈 파일이거나, 선택한 번호에 해당하는 줄에 내용이 없을 경우
       System.out.println("잘못된 번호입니다. 새 할일 내용을 입력하세요:");
+      scanner.nextLine(); // 바로 전 과정에서 Enter를 입력하여 버퍼에 개행 문자가 남아있으므로 개행 문자를 한번 소비해줘야 한다.
       String newStuff = scanner.nextLine(); // 사용자가 입력한 새 할일 내용을 저장하는 변수 선언
       addNewStuff(stuffNumber, newStuff);   // 할일 내용 갱신
     }
